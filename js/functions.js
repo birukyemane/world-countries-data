7c0de4b9983030f28ccdf6e7fe3e2b6530b0903c
birukyemane <bir_yem@yahoo.com>
2018-09-27 21:06:47 +0300

    searching by region added, the search is done by region lookup and search by country name or capital

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,12 @@
-function displayCountry(countries,columns){
+// display the result by generating 
+
+function displayCountry(countries,columns){ // creates a wrapper div for each country 
   return countries.reduce((c,v)=>{
     return c += `<div id="countryWrapper">${cells(v,columns)}</div>`;
   },'')   
 }
 
-function cells(element,columns){ 
+function cells(element,columns){  // creates divs for each country info 
   let info = '', flag = '', additionalColumns = '';
   for(column of columns){
     if(column !== 'flag' && column !== 'currencies' && column !=='languages') {
@@ -18,17 +20,17 @@ function cells(element,columns){
   return `<div id="countryDetails">${info} ${additionalColumns}</div>${flag}`;
 }
 
-function convertArrayKeys(data,column){ // converts the array keys such as languages to html tag
+function convertArrayKeys(data,column){ // converts properties such as languages and currency to html tag
  let htmlTag = data.reduce((a,c,i)=>{
    return a += (i==0?'':', ') + c.name ;
  }
 ,'');    
   return `<div id="${column}"> <span>${column} :</span> ${htmlTag}</div>`;
 }
-// ${(languagesArray? languagesLabel + languagesArray: "")}
-// sort function
-function sortString(key,option){
 
+// sort by country name or capital 
+
+function sortString(key,option){
   if(option=='A'){
     result.sort(function(a, b) {
       var nameA = a[key].toUpperCase(); // ignore upper and lowercase
@@ -61,7 +63,6 @@ function sortString(key,option){
  document.querySelector('#tableContainer').innerHTML = displayCountry(result,slectedColumns);  
 }
 
-
 // sort by population
 function sortPopulation (option){
   if(option == 'A'){
@@ -77,11 +78,7 @@ function sortPopulation (option){
  }
 
 
- // slect columns that usr want to be displayed
-function remove(array, element) {
-  const index = array.indexOf(element);
-  array.splice(index, 1);
-}
+ // add additional column user wants 
 
 function selectColumn(choice){
   let checkboxInput = document.querySelector(`#${choice}`);
@@ -94,25 +91,53 @@ function selectColumn(choice){
   }
 }
 
+// remove an additional column 
+
+function remove(array, element) {
+  const index = array.indexOf(element);
+  array.splice(index, 1);
+}
 
 // search functions
 
 function search() {
+  // search based on the search input   
   let searchKey = document.querySelector('#search').value.toLowerCase();
-  result = worldCountries.filter(e => 
-    e.name.toLowerCase().includes(searchKey) || e.capital.toLowerCase().includes(searchKey)
-  );
-  document.querySelector('#tableContainer').innerHTML= displayCountry(result,slectedColumns); 
+  let choice = document.querySelector('#regions').value.toLowerCase();
+  result = worldCountries;
+  if(searchKey){
+    result = worldCountries.filter(e => 
+      e.name.toLowerCase().includes(searchKey) || e.capital.toLowerCase().includes(searchKey)
+      || includesLanguage(e.languages,searchKey)
+    );
+  } 
+
+  if(choice){
+    result = result.filter(e => e.region.toLowerCase() == choice);
+  }
+ document.querySelector('#tableContainer').innerHTML= displayCountry(result,slectedColumns); 
 }
+
+function includesLanguage(languages,searchWord){
+  searchWord = searchWord.toLowerCase();
+  for(language of languages){
+    if(language.name.toLowerCase() == searchWord){
+      return true;
+    }
+  }
+  return false;
+}
+
+// add the regions to the select input 
 
 function listRegions (){
   let regions = [];
   worldCountries.forEach( country =>{
-    if(!regions.includes(country.region)) regions.push(country.region);
+    if(!regions.includes(country.region) && country.region !== '') regions.push(country.region);
   })
- let options =   regions.map(region => {
-    return `<option>${region}</region>`;
-  }).join('');
-
+  let options = '<option value="">--choose a region--</option>'; 
+  options = options.concat(regions.map(region => {
+    return `<option value="${region}">${region}</region>`;
+  }).join(''));
   document.querySelector('#regions').innerHTML = options;  
 }
